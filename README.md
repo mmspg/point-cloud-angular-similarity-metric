fcc7fab6aa8fcc3b3f799fef685ec06423a846ee
Vaggelis Alexiou <evangelos.alexiou@epfl.ch>
2019-10-15 20:26:23 +0200

    Update references

--- README.md
+++ README.md
@@ -2,14 +2,15 @@
 
 Point clouds denote a 3D content representation which has recently gained a significant amount of interest. Among other challenges, objective quality assessment remains an open problem. Thus, new and efficient algorithms are required, able to predict the level of perceptual impairment of a distorted point cloud with respect to its original version. This algorithm is based on the angular similarity and provides a normal-based alternative approach for objective quality assessment of point cloud contents.
 
-The angular similarity reflects the different orientation of tangent planes that correspond to pairs of points, which are associated as nearest neighbors between point clouds A and B. Tangent planes can be viewed as local linear approximations of the underlying surfaces of the model. Thus, the angular similarity between two tangent planes provides an indication of the difference between local surfaces that pass by the corresponding points. 
+The angular similarity reflects the different orientation of tangent planes that correspond to pairs of points, which are associated as nearest neighbors between two point clouds. Tangent planes can be viewed as local linear approximations of the underlying surfaces of a model. Thus, the angular similarity between two tangent planes provides an indication of the difference between local surfaces that pass by the corresponding points. For more details, please refer to [1].
 
 This metric requires the normals of both the original and the distorted contents. In case there are no normals present in a content by default, they should be estimated before using this script.  
-  
-  
-If you use this code in your research, please cite our paper:
 
-E. Alexiou and T. Ebrahimi, "Point Cloud Quality Assessment Metric Based on Angular Similarity," 2018 IEEE International Conference on Multimedia and Expo (ICME), San Diego, CA, 2018, pp. 1-6. doi: 10.1109/ICME.2018.8486512
+### Conditions of use
 
-URL @ Infoscience: https://infoscience.epfl.ch/record/254987?ln=en <br />
-URL @ IEEEXplore:  https://ieeexplore.ieee.org/document/8486512
+If you wish to use this metric in your research, please cite [1].
+
+
+### References
+
+[1] E. Alexiou and T. Ebrahimi, "[Point Cloud Quality Assessment Metric Based on Angular Similarity](https://ieeexplore.ieee.org/document/8486512)," 2018 IEEE International Conference on Multimedia and Expo (ICME), San Diego, CA, 2018, pp. 1-6. 
--- angularSimilarity.m
+++ angularSimilarity.m
@@ -1,50 +1,55 @@
 function [asBA, asAB, asSym] = angularSimilarity(A, B, ERRORTYPE)
 % Copyright (C) 2018 ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland
-% 
+%
 %     Multimedia Signal Processing Group (MMSPG)
-% 
+%
 %     This program is free software: you can redistribute it and/or modify
 %     it under the terms of the GNU General Public License as published by
 %     the Free Software Foundation, either version 3 of the License, or
 %     (at your option) any later version.
-% 
+%
 %     This program is distributed in the hope that it will be useful,
 %     but WITHOUT ANY WARRANTY; without even the implied warranty of
 %     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 %     GNU General Public License for more details.
-% 
+%
 %     You should have received a copy of the GNU General Public License
 %     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-% 
-% Author: Evangelos Alexiou
-% Email: evangelos.alexiou@epfl.ch
-% 
-% If you use this code in your research, please cite our paper:
-% https://infoscience.epfl.ch/record/254987?ln=en
 %
-% angularSimilarity reflects the different orientation of tangent planes 
-%   that correspond to pairs of points, which are associated as nearest 
+%
+% Author:
+%   Evangelos Alexiou (evangelos.alexiou@epfl.ch)
+%
+% Reference:
+%   E. Alexiou and T. Ebrahimi, "Point Cloud Quality Assessment Metric
+%   Based on Angular Similarity," 2018 IEEE International Conference on
+%   Multimedia and Expo (ICME), San Diego, CA, 2018, pp. 1-6.
+%
+%
+% angularSimilarity reflects the different orientation of tangent planes
+%   that correspond to pairs of points, which are associated as nearest
 %   neighbors between models A and B.
-% 
+%
 %   [asBA, asAB, asSym] = angularSimilarity(A, B, ERRORTYPE)
-% 
+%
 %   INPUTS
-%       A, B: Point clouds in pointCloud format as defined in MATLAB. The 
-%       coordinates (e.g., A.Location) and the normal vectors (e.g., 
+%       A, B: Point clouds in pointCloud format as defined in MATLAB. The
+%       coordinates (e.g., A.Location) and the normal vectors (e.g.,
 %       A.Normal) of the input point clouds A and B must exist.
-%       ERRORTYPE: Defines how the total similarity is computed, based on 
+%       ERRORTYPE: Defines how the total similarity is computed, based on
 %       the functions 'min', 'mean', 'median', 'max', 'RMS', or 'MSE'.
-%   
+%
 %   OUTPUTS
 %       asBA: The total similarity obtained with point cloud A being the
 %       reference. The computed value depends on ERRORTYPE selection.
 %       asAB: The total similarity obtained with point cloud B being the
 %       reference. The computed value depends on ERRORTYPE selection.
-%       asSym: The symmetric total similarity. The computed value depends 
+%       asSym: The symmetric total similarity. The computed value depends
 %       on ERRORTYPE selection.
-%      
-%   Example: 
+%
+%   Example:
 %   [asBA, asAB, asSym] = angularSimilarity(A, B, 'MSE')
+
 
 
 if nargin < 2
@@ -70,7 +75,7 @@ end
 
 
 % Set A as the reference. Loop over B and find nearest neighbor in A
-[n1, ~] = knnsearch(A.Location, B.Location); 
+[n1, ~] = knnsearch(A.Location, B.Location);
 as_BA = (pi - 2*acos(abs(sum(A.Normal(n1,:).*B.Normal,2))))/pi;
 as_BA(isnan(as_BA)) = [];
 
@@ -91,7 +96,7 @@ else
 end
 
 % Set B as the reference. Loop over A and find nearest neighbor in B
-[n2, ~] = knnsearch(B.Location, A.Location); 
+[n2, ~] = knnsearch(B.Location, A.Location);
 as_AB = (pi - 2*acos(abs(sum(B.Normal(n2,:).*A.Normal,2))))/pi;
 as_AB(isnan(as_AB)) = [];
 
@@ -113,5 +118,3 @@ end
 
 % Symmetric total similarity
 asSym = min(asBA, asAB);
-
-
